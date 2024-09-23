c836966ebe98c4766a5f0ef54abe9cd4087534e9
tr1pleseven <lucasshalbe@gmail.com>
2024-09-23 21:59:51 +0200

    commit3
    Submit done, displaying input data done

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-let submitemail = document.querySelector('#subemail')
-let submitpassword = document.querySelector('submitpassword')
+let submit = document.querySelectorAll('.submitform')
+
+let inputemail = document.querySelector('#inputemail')
+let inputpassword = document.querySelector('#inputpassword')
+
+submit.forEach(elem => elem.addEventListener("click", fn))
+
+function fn(e){
+    e.preventDefault()
+    console.log(`${inputemail.value}:${inputpassword.value}`)
+    inputemail.value = ''
+    inputpassword.value = ''
+}
