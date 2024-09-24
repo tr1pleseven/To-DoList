c85e9425426536d83c1e36cc196c95b51809de14
tr1pleseven <lucasshalbe@gmail.com>
2024-09-24 18:57:27 +0200

    update

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ submit.forEach(elem => elem.addEventListener("click", fn))
 
 function fn(e){
     e.preventDefault()
-    console.log(`${inputemail.value}:${inputpassword.value}`)
+    console.log(`${inputemail.value}_${inputpassword.value}`)
+    
     inputemail.value = ''
     inputpassword.value = ''
+    function thisperson[](email,password){
+      this.password = password
+      this.email = email
+    }
 }
