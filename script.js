ee39f09bc680a97fd19f38055d43190d8749f7fc
tr1pleseven <lucasshalbe@gmail.com>
2024-09-27 21:28:53 +0200

    Revamp(todo)

--- script.js
+++ script.js
@@ -1,18 +1,60 @@
-let submit = document.querySelectorAll('.submitform')
+//variables
+let todoinput = document.querySelector(".inputtodo")
+let submitbutton = document.querySelector(".btn")
+let _ul = document.querySelector("ul")
 
-let inputemail = document.querySelector('#inputemail')
-let inputpassword = document.querySelector('#inputpassword')
+let clearlistbtn = document.getElementById("clearlist")
 
-submit.forEach(elem => elem.addEventListener("click", fn))
 
-function fn(e){
+//get date
+const now = new Date()
+
+
+//event listeners
+submitbutton.addEventListener("click", clickevent)
+todoinput.addEventListener("input" , inputevent)
+clearlistbtn.addEventListener("click", clearlist)
+
+
+function clearlist(e){
+   _ul.innerHTML = ""
+}
+
+
+
+function clickevent(e){
+
+    //assign time
+    const time = now.getDay()
+
+e.preventDefault()
+var li = document.createElement("li")
+
+//_CHILD_APPEND_LIST
+li.appendChild(document.createTextNode(todoinput.value))
+
+var newdiv1 = li.appendChild(document.createElement("div"))
+var lilabel = newdiv1.appendChild(document.createElement("label"))
+lilabel.className = "Progress"
+lilabel.textContent = "Done:"
+
+
+
+var gg = lilabel.appendChild(document.createElement("input"))
+gg.setAttribute("type","checkbox")
+console.log(time)
+
+li.className = "todo"
+
+
+_ul.appendChild(li)
+todoinput.value = null
+
+
+
+}
+
+function inputevent(e){
     e.preventDefault()
-    console.log(`${inputemail.value}_${inputpassword.value}`)
-    
-    inputemail.value = ''
-    inputpassword.value = ''
-    function thisperson(email,password){
-      this.password = password
-      this.email = email
-    }
+    console.log(todoinput.value)
 }
